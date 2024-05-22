56fc186176375d492da2be7ff147d97b2c925473
Mehek Kaur <166215239+mehek3@users.noreply.github.com>
2024-05-22 18:29:21 +0530

    Add files via upload

---
+++ StopWatch.jsx
@@ -0,0 +1,21 @@
+// src/components/Stopwatch.jsx
+
+import { useState, useEffect } from "react";
+
+function StopWatch() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);}, 1000);
+  }, []);
+
+  return (
+    <div style={{ border: "1px dashed red", padding: "10px", margin: "10px" }}>
+      <h1>Stop Watch</h1>
+      <h4>{count}</h4>
+    </div>
+  );
+}
+
+export default StopWatch;
